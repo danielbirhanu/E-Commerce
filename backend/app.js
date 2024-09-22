459649562414ec72194e943bce30ce6b9c10f17e
danielbirhanu <danielbir49@gmail.com>
2024-09-22 18:32:48 +0300

    configured it for deployment

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,7 @@ const port = process.env.PORT;
 connectDB();
 
 const allowedOrigins = [
-  "https://main.d1sj7cd70hlter.amplifyapp.com",
-  "https://expense-tracker-app-three-beryl.vercel.app",
+  "http://localhost:3000",
   // add more origins as needed
 ];
 
